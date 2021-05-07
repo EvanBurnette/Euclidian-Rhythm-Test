e815c011533e6d1631c879a108a4f380c8462f04
Evan Burnette <EvanBurnette@users.noreply.github.com>
2021-05-07 00:24:35 -0400

    Bresian algorithm half working

--- euclidianRhythm.js
+++ euclidianRhythm.js
@@ -1,17 +1,30 @@
 function euc(onsets, pulses) {
-    let onsArr = []
-    onsArr.length = onsets
-    onsArr.fill(true)
+    let timingsArr = []
+    for (let i = 0; i < onsets; i++){
+            timingsArr.push(Math.ceil(i*pulses/onsets))
+    }
+    
+    let solutionArr = []
+    solutionArr.length = pulses
+    solutionArr.fill('0')
 
-    let offArr = []
-    offArr.length = pulses - onsets
-    offArr.fill(false)
+    timingsArr.forEach(item => {
+        solutionArr[item] = '1'
+    })
+
+    return solutionArr.join('')
+    // let onsArr = []
+    // onsArr.length = onsets
+    // onsArr.fill(true)
+
+    // let offArr = []
+    // offArr.length = pulses - onsets
+    // offArr.fill(false)
      
-    let startArr = onsArr.concat(offArr)
-    return recEuc(onsArr, offArr)
+    // let startArr = onsArr.concat(offArr)
 }
 
-function recEuc() {
-    return [true, false, true]
-}
+// function recEuc() {
+//     return [true, false, true]
+// }
 module.exports = euc
--- euclidianRhythm.test.js
+++ euclidianRhythm.test.js
@@ -3,12 +3,12 @@ const euc = require('./euclidianRhythm')
 const euclidTestArr = require('./euclidianTestSet.json')
 
 euclidTestArr.forEach((rhythm) => {
-    rhythm.solutionArr = rhythm.solution.split('').map((bin) => bin == '1' ? true : false)
+    rhythm.solutionArr = rhythm.solution.split('').map((bin) => Number.parseInt(bin))
 })
 
 // console.table(euclidTestArr)
 euclidTestArr.forEach(rhythm => {
     test('euclidian rhythms should match', () => {
-        expect(euc(rhythm.onsets, rhythm.pulses)).toEqual(rhythm.solutionArr)
+        expect(euc(rhythm.onsets, rhythm.pulses)).toEqual(rhythm.solution)
     })
 })
